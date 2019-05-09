c55130630edd0d5d329179d158a9ace127274857
Diana Mryhlotska <diana.mryhlotska.kn.2016@lpnu.ua>
2019-05-09 05:08:39 +0300

    Login form

--- edoctor-app/src/app/auth/auth.module.ts
+++ edoctor-app/src/app/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { MaterialsModule } from '../materials/materials.module';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { AuthComponent } from './auth.component';
+import { ParticlesModule } from 'angular-particle';
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent, AuthComponent],
@@ -14,6 +15,7 @@ import { AuthComponent } from './auth.component';
     SharedModule,
     MaterialsModule,
     AuthRoutingModule,
+    ParticlesModule
   ]
 })
 export class AuthModule { }
--- edoctor-app/src/app/auth/components/login/login.component.ts
+++ edoctor-app/src/app/auth/components/login/login.component.ts
@@ -1,15 +1,89 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
+  
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  myStyle: object = {};
+    myParams: object = {};
+    width: number = 100;
+    height: number = 100;
+  public logForm: FormGroup;
+  constructor(private fb: FormBuilder,
+              private authService: AuthService) { }
+
+
+
+  initLogForm() {
+    this.logForm = this.fb.group({
+      first_name: [null, Validators.required],
+      last_name: [null, Validators.required],
+      username: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      gender: [null, Validators.required],
+      role: [null, Validators.required],
+      password: [null, [Validators.required],Validators.minLength(8)],
+      hospital: [null, Validators.required]
+    });
+  }
+
 
   ngOnInit() {
+    this.myStyle = {
+      'position': 'fixed',
+      'width': '100%',
+      'height': '100%',
+      'z-index': -1,
+      'top': 0,
+      'left': 0,
+      //"value": 20,
+      'right': 0,
+      'bottom': 0,
+      'speed': 2,
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": false
+          }
+        }
+      },
+      "modes": {
+        "push": {
+          "particles_nb": 12
+        }
+      },
+      
+    };
+
+this.myParams = {
+      particles: {
+          number: {
+              value: 250,
+          },
+          color: {
+              value: '#ff0000'
+          },
+          shape: {
+              type: 'triangle',
+              "stroke": {
+                "width": 5,
+                "color": "#fff00"
+             },
+             "triangle": {
+                "nb_sides": 10
+             }
+          },
+  }
+};
+    this.initLogForm();
   }
 
 }
